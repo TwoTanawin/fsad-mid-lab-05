211b5c68662bca1c123eaab373973d1e96f31c24
Tanawin Siriwan <two-mac@Tanawins-MacBook-Air.local>
2024-09-22 19:36:08 +0700

    lab-05 MVC

--- app/config/routes.rb
+++ app/config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :students
+  # resources :projects
+  # resources :students
+
+  resources :projects do
+    resources :students
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -12,4 +17,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  root "projects#index"
 end
--- app/db/schema.rb
+++ app/db/schema.rb
@@ -10,8 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 0) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_22_064944) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "projects", force: :cascade do |t|
+    t.string "name"
+    t.string "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string "studentid"
+    t.string "name"
+    t.bigint "project_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_students_on_project_id"
+  end
+
+  add_foreign_key "students", "projects"
 end
